6f55b535747db05e208cef97b33ee249dc86c717
Marcelina Hołub <mholub@tutanota.com>
2024-06-19 13:17:26 +0200

    feat(api): wrap FileKey

---
+++ src/caged/api/file_key.clj
@@ -0,0 +1,58 @@
+(ns caged.api.file-key
+  (:import (com.exceptionfactory.jagged FileKey))
+  (:require [clojure.java.io :as io]))
+
+(defn create-file-key
+  "Creates a new key file"
+  []
+  (FileKey.))
+
+(defn write-key-to-file
+  "Creates a new key file and writes it to the specified file"
+  [path]
+  (let [file-key (create-file-key)]
+    (.writeToFile file-key (io/file path))))
+
+(defn create-file-key-from-bytes
+  "Creates a new key file from a byte array"
+  [byte-array]
+  (FileKey. byte-array))
+
+(defn get-algorithm
+  "Returns the algorithm used to generate the key.
+  Age supported algorithms are:
+  - ChaCha20-Poly1305
+  - HmacSHA256
+  - PBKDF2WithHmacSHA256
+  - RSA/ECB/OAEPPadding
+  - X25519"
+  [file-key]
+  (.getAlgorithm file-key))
+
+(defn get-format
+  "Returns the format of the key file. This is the format of the key file, not the key itself."
+  [file-key]
+  (.getFormat file-key))
+
+(defn get-encoded
+  "Returns the encoded bytes of the key file."
+  [file-key]
+  (.getEncoded file-key))
+
+(defn destroy
+  "Destroys the key file."
+  [file-key]
+  (.destroy file-key))
+
+(defn is-destroyed? [file-key]
+  "Returns true if the key file has been destroyed.
+  Destroyed key files cannot be used to perform cryptographic operations."
+  (.isDestroyed file-key))
+
+(defn file-key-eq? [file-key1 file-key2]
+  "Compares two key files for equality."
+  (.equals file-key1 file-key2))
+
+(defn hash-code [file-key]
+  "Returns the hash code of the key file."
+  (.hashCode file-key))
